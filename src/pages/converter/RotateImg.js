5cfbe3304422362801e371befa4de505b99b6185
keyushlogicgo <keyush.logicgo@gmail.com>
2023-04-22 15:22:46 +0530

    update code

--- src/pages/converter/RotateImg.js
+++ src/pages/converter/RotateImg.js
@@ -3,13 +3,22 @@ import { Card, Col, Row } from "react-bootstrap";
 import useHelper from "../../hook/useHelper";
 
 const RotateImg = () => {
-  const { singleDownload, base64ToBlob } = useHelper();
+  const {
+    singleDownload,
+    base64ToBlob,
+    handlePhoto,
+    handleRemove,
+    zipDownload,
+    photo,
+  } = useHelper();
   const [image, setImage] = useState({
     file: "",
     url: "",
   });
   const [outputImg, setOutputImg] = useState("");
   const [rottedCtn, setRottedCtn] = useState(0);
+  const [isRotated, setIsRotated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFile = (input) => {
     if (input.files && input.files[0]) {
@@ -22,61 +31,30 @@ const RotateImg = () => {
     }
   };
 
-  const handleRotateImg = async (file) => {
-    // const imgData = await rotatedImage(image.file);
-    // const newFile = await base64ToBlob(imgData);
-    // image.file = newFile;
-    // setOutputImg(imgData);
+  const handleRotateImg = async () => {
     setRottedCtn(rottedCtn < 3 ? rottedCtn + 1 : 0);
   };
-
-  const handlePreviewFile = async () => {
+  var convertCtn = 0;
+  const handleConvert = () => {
+    setIsLoading(true);
+    console.time();
     var degrees = rottedCtn * 90;
-    const newData = await rotateImage(degrees, image.file);
-    setOutputImg(newData);
+    var filesArr = Array.prototype.slice.call(photo);
+
+    filesArr.forEach(async (item, key) => {
+      const newData = await rotateImage(degrees, item.file);
+      const newFile = await base64ToBlob(newData);
+      item.file = newFile;
+      convertCtn++;
+      if (filesArr.length === convertCtn) {
+        console.timeEnd();
+        setIsRotated(true);
+        setIsLoading(false);
+      }
+    });
   };
-  const handleDownloadFile = async () => {
-    const newFile = await base64ToBlob(outputImg);
-    
-    singleDownload(newFile);
-  };
-
-  // const rotatedImage = (file) => {
-  //   return new Promise((resolve, reject) => {
-  //     const img = new Image();
-  //     img.onload = function () {
-  //       const canvas = document.createElement("canvas");
-  //       canvas.width = this.width;
-  //       canvas.height = this.height;
-  //       const ctx = canvas.getContext("2d");
-
-  //       setRottedCtn(rottedCtn < 4 ? rottedCtn + 1 : 1);
-  //       var x = 0;
-  //       var y = 0;
-  //       var w = this.width;
-  //       var h = this.height;
-
-  //       console.log("go", rottedCtn);
-
-  //       var degrees = (rottedCtn - 1) * 90;
-  //       ctx.save();
-  //       ctx.translate(x + w / 2, y + h / 2);
-  //       ctx.rotate(0.017453292519943295 * degrees);
-  //       ctx.translate(-x - w / 2, -y - h / 2);
-  //       ctx.drawImage(this, x, y, w, h);
-  //       ctx.restore();
-  //       const dataUrl = canvas.toDataURL("image/jpeg");
-  //       resolve(dataUrl);
-  //     };
-  //     img.onerror = function () {
-  //       reject(new Error("Failed to load image"));
-  //     };
-  //     img.src = URL.createObjectURL(file);
-  //   });
-  // };
 
   function rotateImage(degree, file) {
-    console.log(degree, file);
     return new Promise((resolve, reject) => {
       const img = new Image();
       img.onload = function () {
@@ -104,7 +82,7 @@ const RotateImg = () => {
             cx = img.width * -1;
             break;
         }
-        console.log("degree", degree);
+
         // Rotate image
         canvas.setAttribute("width", cw);
         canvas.setAttribute("height", ch);
@@ -121,15 +99,14 @@ const RotateImg = () => {
     });
   }
 
-  console.log(rottedCtn);
-
   return (
     <>
       <input
         type="file"
         id="cropFile"
+        multiple
         onChange={(e) => {
-          handleFile(e.target);
+          handlePhoto(e.target);
         }}
         hidden
       />
@@ -145,54 +122,84 @@ const RotateImg = () => {
         >
           Rotate
         </button>
-        <button
-          onClick={(e) => {
-            handlePreviewFile();
-          }}
-          type="button"
-          className="btn btn-secondary me-2"
-        >
-          Preview
-        </button>
-        <button
-          onClick={(e) => {
-            handleDownloadFile(image.file);
-          }}
-          type="button"
-          className="btn btn-secondary"
-        >
-          Download
-        </button>
       </div>
       {JSON.stringify(image) !== "{}" ? (
         <>
-          <Card className="py-5">
+          <Card>
             <Card.Body>
               <Row>
-                <Col md={6}>
-                  {image.url !== "" ? (
-                    <img
-                      src={image.url}
-                      className=" border object-contain h-500"
-                      alt="..."
-                      id="previewImage"
-                      style={{
-                        transform: `rotate(${rottedCtn * 90}deg)`,
-                      }}
-                    />
-                  ) : null}
-                </Col>
-                <Col md={6}>
-                  {outputImg !== "" ? (
-                    <img
-                      src={outputImg}
-                      className="w-100 h-500 border object-contain"
-                      alt="..."
-                    />
-                  ) : null}
-                </Col>
+                {photo?.map((item, key) => {
+                  return (
+                    <Col
+                      xl={2}
+                      lg={3}
+                      md={4}
+                      sm={6}
+                      xs={12}
+                      key={key}
+                      className="mb-3"
+                    >
+                      <Card>
+                        <Card.Body className="p-0">
+                          <button
+                            onClick={(e) => {
+                              handleRemove(item.id);
+                            }}
+                            type="button"
+                            className="btn btn-danger hw-42 position-absolute end-0 top-0"
+                          >
+                            -
+                          </button>
+
+                          <img
+                            className="h-200 w-100 object-contain"
+                            alt="..."
+                            style={{
+                              transform: `rotate(${rottedCtn * 90}deg)`,
+                            }}
+                            src={item.url}
+                          />
+                          <p className="text-center fs-14 mb-0 p-1">
+                            {item.name}
+                          </p>
+
+                          <button
+                            onClick={(e) => {
+                              singleDownload(item.file);
+                            }}
+                            type="button"
+                            className="btn btn-secondary w-100"
+                          >
+                            Download
+                          </button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
               </Row>
             </Card.Body>
+            <Card.Footer>
+              {isLoading ? (
+                <span className="btn w-100 btn-secondary">Loading...</span>
+              ) : !isRotated ? (
+                <button
+                  type="button"
+                  className="btn btn-secondary w-100"
+                  onClick={handleConvert}
+                >
+                  Convert
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-secondary w-100"
+                  onClick={() => zipDownload(photo, "myfile", "")}
+                >
+                  Download All
+                </button>
+              )}
+            </Card.Footer>
           </Card>
         </>
       ) : null}
